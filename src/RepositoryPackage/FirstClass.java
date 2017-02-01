5e113a34e5d0902083d9d9976d5b1ae5f261062c
seantalo <sean.talo@pcc.edu>
2017-02-01 14:19:13 -0800

    Added a Main method that prints some numeric type value ranges

--- src/RepositoryPackage/FirstClass.java
+++ src/RepositoryPackage/FirstClass.java
@@ -1,8 +1,15 @@
 package RepositoryPackage;
 
 /**
- * Created by bitraven on 2/1/2017.
+ * Created by seantalo on 2/1/2017.
  */
 public class FirstClass
 {
+    public static void main(String[] args)
+    {
+        System.out.println("Valid integers are between " + Integer.MIN_VALUE + " and " + Integer.MAX_VALUE );
+        System.out.println("Valid bytes are between " + Byte.MIN_VALUE + " and " + Byte.MAX_VALUE );
+        System.out.println("Valid shorts are between " + Short.MIN_VALUE + " and " + Short.MAX_VALUE );
+        System.out.println("Valid longs are between " + Long.MIN_VALUE + " and " + Long.MAX_VALUE );
+    }
 }
